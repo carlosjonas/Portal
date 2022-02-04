6089fd523b8d9107eae989b341b6096b3c014a32
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-04 17:57:40 -0300

    feature: criando o cadastrar

---
+++ cadastrar.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use \App\Entity\Noticia;
+
+// Validação de campos do formulário 
+if (isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])) {
+
+	$noticia = new Noticia;
+	$noticia->titulo = $_POST['titulo'];
+	$noticia->descricao = $_POST['descricao'];
+	$noticia->ativo = $_POST['ativo'];
+	$noticia->cadastrar();
+
+	echo "<pre>"; print_r($noticia); echo "</pre>"; exit;
+
+}
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/formulario.php';
+include __DIR__.'/includes/footer.php';
+
+?>
