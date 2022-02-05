b1ba3505da13aa9401bd16a1dbf94ad648fa4137
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-05 10:25:52 -0300

    refactor: trazendo a listagem de notícias

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,9 +1,35 @@
-<main>
-	
+<main>	
 	<section>
 		<a href="cadastrar.php">
 			<button class="btn btn-success">Nova Notícia</button>
 		</a>
 	</section>
 
+	<section>
+		<?php 
+			$resultados = '';
+			foreach ($noticia as $noticias):
+		?>
+			<div class="card mb-3 mt-3">
+			  <img src="..." class="card-img-top" alt="...">
+			  <div class="card-body text-dark">
+			    <h5 class="card-title">
+			    	<?= $noticias->titulo ;?>
+			    </h5>
+			    <p class="card-text"><?= $noticias->descricao ;?></p>
+			    <p class="card-text"><small class="text-muted"><?= date('d/m/Y à\s H:i:s',strtotime($noticias->data)) ;?></small></p>
+			  </div>
+			  <div class="card-body text-dark">
+			  	<a href="editar.php?id='<?= $noticias->id ;?>'">
+			  		<button type="button" class="btn btn-primary">Editar</button>
+			  	</a>
+			  	<a href="excluir.php?id='<?= $noticias->id ;?>'">
+			  		<button type="button" class="btn btn-danger">Excluir</button>
+			  	</a>
+			  </div>
+			</div>
+		<?php 
+			endforeach;
+ 		?>
+	</section>
 </main>
