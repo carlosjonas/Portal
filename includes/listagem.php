1b09e21411e80302ea58ef4579ccd8dcfb386572
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-05 16:47:57 -0300

    refactor: terminando de trazer a listagem de notícias

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,5 +1,20 @@
-<main>	
+<main>
+<?php 
+	$mensagem = '';
+			if (isset($_GET['status'])) {
+				switch ($_GET['status']) {
+					case 'success':
+						$mensagem= '<div class="alert alert-success">Ação executada com sucesso!</div>';
+						break;
+					
+					case 'error':
+						$mensagem= '<div class="alert alert-danger">Ação não executada!</div>';
+						break;
+				}
+			}
+ ?>	
 	<section>
+		<?= $mensagem; ?>
 		<a href="cadastrar.php">
 			<button class="btn btn-success">Nova Notícia</button>
 		</a>
@@ -8,13 +23,16 @@
 	<section>
 		<?php 
 			$resultados = '';
+			$resultados = strlen($resultados) ? $resultados : '<p>Nenhuma Nótícia publicada!</p>';
 			foreach ($noticia as $noticias):
-		?>
+		?>	
+			<?= $resultados; ?>
+			
 			<div class="card mb-3 mt-3">
 			  <img src="..." class="card-img-top" alt="...">
 			  <div class="card-body text-dark">
 			    <h5 class="card-title">
-			    	<?= $noticias->titulo ;?>
+			    	<?= $noticias->id ;?> - <?= $noticias->titulo ;?>
 			    </h5>
 			    <p class="card-text"><?= $noticias->descricao ;?></p>
 			    <p class="card-text"><small class="text-muted"><?= date('d/m/Y à\s H:i:s',strtotime($noticias->data)) ;?></small></p>
@@ -30,6 +48,7 @@
 			</div>
 		<?php 
 			endforeach;
- 		?>
+			
+ 		?>	
 	</section>
 </main>
