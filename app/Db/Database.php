54c3e1ddfe464d88300aeb841e1378470d761025
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-05 16:44:14 -0300

    refactor: adicionando novos metodos para o banco

--- app/Db/Database.php
+++ app/Db/Database.php
@@ -67,9 +67,9 @@ class Database{
 	//Método que faz consulta no banco
 	public function select($where = null, $order = null, $limit = null){
 		//Dados da query
-		$where = strlen($where) ? 'WHERE '.$where : '';
-		$order = strlen($order) ? 'ORDER BY '.$order : '';
-		$limit = strlen($limit) ? 'LIMIT '.$limit : '';
+		$where = strlen($where) ? ' WHERE '.$where : '';
+		$order = strlen($order) ? ' ORDER BY '.$order : '';
+		$limit = strlen($limit) ? ' LIMIT '.$limit : '';
 
 		//Monta a query
 		$query = 'SELECT * FROM '.$this->table.''.$where.''.$order.''.$limit;
@@ -78,7 +78,33 @@ class Database{
 		return $this->execute($query);
 	}
 
+	// Método que atualiza os dados no banco
+	public function update($where,$values){
+		//Dados da query
+		$fields = array_keys($values);
 
+		//Monta a query
+		$query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+
+		//Executa a query
+		$this->execute($query,array_values($values));
+
+		//Retorno
+		return true;
+	}
+
+	//Método que deleta os dados do banco
+	public function delete($where){
+
+		//Monta a query
+		$query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+
+		//Executa a query
+		$this->execute($query);
+
+		//Retorno
+		return true;
+	}
 
 
 }
