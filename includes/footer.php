3d5079362976c3ddfb2414313dad55265b450157
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-07 16:34:53 -0300

    refactor: colocando footer na página

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,107 @@
     <!-- Classe container abaixo do body -->
     </div>
+    <div class="container-fluid">
+        <div class="jumbotron bg-primary text-light" id="footer">
+            <section style="margin-top: 50px;">
+                <!-- Footer -->
+            <footer class="text-center   ">
+              <!-- Section: Social media -->
+              <section
+                class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+              >
+                <!-- Left -->
+                <div class="me-5 d-none d-lg-block">
+                  <span>Conecte-se conosco nas redes sociais:</span>
+                </div>
+                <!-- Left -->
+                <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+                <!-- Facebook -->
+            <a class="btn btn-primary" style="background-color: #3b5998;" href="https://www.facebook.com/OPOVO" role="button"
+            ><i class="fab fa-facebook-f"></i
+            ></a>
 
+            <!-- Twitter -->
+            <a class="btn btn-primary" style="background-color: #55acee;" href="https://twitter.com/opovo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" role="button"
+            ><i class="fab fa-twitter"></i
+            ></a>
+
+            <!-- Google -->
+            <a class="btn btn-primary" style="background-color: #dd4b39;" href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiR0N60ju71AhWLEbkGHYafDPYQFnoECAwQAQ&url=https%3A%2F%2Fwww.opovo.com.br%2F&usg=AOvVaw2ceJHALrC7ntd4csfIvUeE" role="button"
+            ><i class="fab fa-google"></i
+            ></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-primary" style="background-color: #ac2bac;" href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi62by9ju71AhX0ILkGHXGJAEwQFnoECAQQAQ&url=https%3A%2F%2Fwww.instagram.com%2Fopovoonline%2F%3Fhl%3Dpt-br&usg=AOvVaw2nYcuheHBck4TCHu62mba5" role="button"
+            ><i class="fab fa-instagram"></i
+            ></a>
+
+
+                <!-- Right -->
+                <div>
+                  <!--ICONES AQUI-->
+                </div>
+                <!-- Right -->
+              </section>
+              <!-- Section: Social media -->
+
+              <!-- Section: Links  -->
+              <section class="">
+                <div class="container text-center text-md-start mt-5">
+                  <!-- Grid row -->
+                  <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                      <!-- Content -->
+                      <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Portal de Notícias
+                      </h6>
+                      <p>
+                        Aqui você acompanha as principais notícias do que acontece no mundo, tudo feito com muito carinho e responsabilidade em levar notícias de qualidade 
+                      </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                      <!-- Links -->
+                      <h6 class="text-uppercase fw-bold mb-4">
+                        Contato
+                      </h6>
+                      <p><i class="fas fa-home me-3"></i>Av. Aguanambi, 282 - José Bonifacio, Fortaleza - CE, 60055-402</p>
+                      <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        portal.noticias@contato.com.br
+                      </p>
+                      <p><i class="fas fa-phone me-3"></i>0800 890 6767</p>
+                    </div>
+                    <!-- Grid column -->
+                  </div>
+                  <!-- Grid row -->
+                </div>
+              </section>
+              <!-- Section: Links  -->
+
+              <!-- Copyright -->
+              <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+                © 2021 Copyright:
+                <a class="text-reset fw-bold" href="index.php">portalnoticias.com</a>
+              </div>
+              <!-- Copyright -->
+            </footer>
+            <!-- Footer -->
+            </section>
+        </div>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+    <!-- Link FontAwesome -->
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+    <!-- Scripts -->
+    <script src="assets/script/scripts.js"></script>
+
   </body>
 </html>
