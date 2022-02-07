c590d8bb85332884c97664f0b98d35d4878a6096
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-02-07 16:35:34 -0300

    refactor: alterando a função de listar

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,21 +1,24 @@
 <main>
+
 <?php 
+	// Verificando o status da url para mandar a mensagem de sucesso
 	$mensagem = '';
 			if (isset($_GET['status'])) {
 				switch ($_GET['status']) {
 					case 'success':
-						$mensagem= '<div class="alert alert-success id="alert" alert-dismissible">Ação executada com sucesso!
+						$mensagem= '<div class="alert alert-success text-center" id="alert">Ação executada com sucesso!
 										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 									</div>';
 						break;
 					case 'error':
-						$mensagem= '<div class="alert alert-danger alert-dismissible">Ação não executada!<
+						$mensagem= '<div class="alert alert-danger text-center">Ação não executada!<
 										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
 									/div>';
 						break;
 				}
 			}
  ?>	
+ 
 	<section>
 		<?= $mensagem; ?>
 		<a href="cadastrar.php">
@@ -29,23 +32,27 @@
 			$resultados = strlen($resultados) ? $resultados : '<p>Nenhuma Nótícia publicada!</p>';
 			foreach ($noticia as $noticias):
 		?>	
-			
-			<div class="card mb-3 mt-3">
-			  <img src="<?= $noticias->imagem ;?>" class="card-img-top">
-			  <div class="card-body text-dark">
-			    <h5 class="card-title">
-			    	<?= $noticias->id ;?> - <?= $noticias->titulo ;?>
-			    </h5>
-			    <p class="card-text"><?= $noticias->descricao ;?></p>
-			    <p class="card-text"><small class="text-muted"><?= date('d/m/Y à\s H:i:s',strtotime($noticias->data)) ;?></small></p>
-			  </div>
-			  <div class="card-body text-dark">
-			  	<a href="editar.php?id='<?= $noticias->id ;?>'">
-			  		<button type="button" class="btn btn-primary">Editar</button>
-			  	</a>
-			  	<a href="excluir.php?id='<?= $noticias->id ;?>'">
-			  		<button type="button" class="btn btn-danger">Excluir</button>
-			  	</a>
+
+			<div class="card mb-3 mt-4" style="width: 100%;">
+			  <div class="row g-0">
+			    <div class="col-md-4">
+			      <img src="<?= $noticias->imagem ;?>" id="imagem" class="img-fluid rounded-start" alt="">
+			    </div>
+			    <div class="col-md-8">
+			      <div class="card-body text-dark" id="info_noticia">
+			        <h5 class="card-title"><?= $noticias->id ;?> - <?= $noticias->titulo ;?></h5>
+			        <p class="card-text"><?= $noticias->descricao ;?></p>
+			        <p class="card-text"><small class="text-muted"><?= date('d/m/Y à\s H:i:s',strtotime($noticias->data)) ;?></small></p>
+			      </div>
+			      <div class="card-body">
+			      	<a href="editar.php?id='<?= $noticias->id ;?>'">
+			  			<button type="button" class="btn btn-primary">Editar</button>
+				  	</a>
+				  	<a href="excluir.php?id='<?= $noticias->id ;?>'">
+				  		<button type="button" class="btn btn-danger">Excluir</button>
+				  	</a>
+			      </div>
+			    </div>
 			  </div>
 			</div>
 		<?php 
@@ -53,4 +60,5 @@
 			
  		?>	
 	</section>
+
 </main>
