4f31ac59b6581d9082a7640fb2f04854743a024e
carlosjonas <carlosjonasfon@gmail.com>
2024-03-11 22:59:24 -0300

    refactor: ajustando oc códigos para as novas páginas

--- includes/header.php
+++ includes/header.php
@@ -6,15 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
 
-    <!-- Font Awesome -->
-    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
-
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS and ICONS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Style CSS -->
--- index.php
+++ index.php
@@ -1,12 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-require __DIR__.'/vendor/autoload.php';
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-use \App\Entity\Noticia;
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+    <link rel="manifest" href="/site.webmanifest">
 
-$noticia = Noticia::getNoticias();
-include __DIR__.'/includes/header.php';
-include __DIR__.'/includes/listagem.php';
-include __DIR__.'/includes/footer.php';
+    <!-- Bootstrap CSS and ICONS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-?>
+    <!-- Style CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/styleLogin.css">
+
+    <title>Login</title>
+
+</head>
+
+<body class="bg-dark text-light">
+    
+        <div class="justify-content-center" style="margin-top: 150px;">
+            
+            <form name="formindex2" class="form-signin" role="form" method="post" action="acesso.php">
+                <div class="row justify-content-center text-center mb-3">
+					<div class="col-6 "> 
+                        <h2>Portal de Notícias</h2>
+                    </div>
+			    </div>
+				<div class="row">
+					<div class="col-6 verticalLine align-self-end">
+						<div>
+							<img class="img-responsive" src="assets/img/logo.png" alt="logotipo.png" title="logotipo.png"/>
+						</div>
+					</div>
+					<div class="col-3 colunaInput">
+						<br>
+						<div class="divInputs">
+							<input id='email' name='email' type="text" class="form-control" placeholder="Email" required>		
+						</div>
+						<div class="divInputs">
+							<input id='senha' name='senha' type="password" maxlength="8" class="form-control" placeholder="Senha" required>
+						</div>
+						<div class="col-xs-12 text-left divBtnSuccess">
+							<button class="btn btn-primary btnSuccess" type="submit">Entrar</button>
+						</div>
+                        <div class="col-xs-12 text-left divBtnSuccess">
+							<a class="btn btn-primary btnSuccess" href="cadastrarUsuarios.php">Cadastre-se</a>
+						</div>
+					</div>
+				</div>
+                    
+            </form>
+        </div>
+	</body>
+
+</html>
