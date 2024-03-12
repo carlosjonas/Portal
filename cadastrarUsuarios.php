9077909091f644c2f5fb60cba17177814d37f0a5
carlosjonas <carlosjonasfon@gmail.com>
2024-03-11 22:57:44 -0300

    feature: criacao da página de cadastro de usuarios

---
+++ cadastrarUsuarios.php
@@ -0,0 +1,43 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+define('TITLE','Cadastrar Usuário');
+
+use \App\Entity\Usuario;
+$usuario = new Usuario();
+
+// Validação de campos do formulário 
+if (isset($_POST['nome'],$_POST['email'],$_POST['rg'],$_POST['cpf'],$_POST['senha'])) {
+
+
+	//Definindo a imagem da notícia
+	$dir = "image/";
+    if(isset($_FILES['imagem'])){
+        $file = $_FILES['imagem'];
+        $destino = "$dir".$file['name'];
+        move_uploaded_file($file['tmp_name'],$destino);
+        
+        
+        $usuario->imagem = $destino;
+    }
+
+    //Inserção de campos
+	$usuario->nome = $_POST['nome'];
+	$usuario->email = $_POST['email'];
+	$usuario->rg = $_POST['rg'];
+    $usuario->cpf = $_POST['cpf'];
+    $usuario->senha = $_POST['senha'];
+    $usuario->tipo = 'l';
+	$usuario->cadastrar();
+
+	header('location: index.php?status=success');
+	exit;
+
+}
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/formularioUsuarios.php';
+include __DIR__.'/includes/footer.php';
+
+?>
