4c03d2809d82080ab17a52d2c8c163e34023f234
carlosjonas <carlosjonasfon@gmail.com>
2024-03-13 22:57:09 -0300

    refactor: ajustando imagem de usuário

--- acesso.php
+++ acesso.php
@@ -2,16 +2,21 @@
 
 require __DIR__.'../vendor/autoload.php';
 
+session_start();
+
 use \App\Entity\Usuario;
 if(isset($_POST["email"])){
     $usuario = Usuario::getUsuario($_POST["email"]);
     
     if(password_verify($_POST['senha'],$usuario->senha)){
-        $_SESSION['idUsuario'] = $usuario->id;
-        $_SESSION['nomeUsuario'] = $usuario->nome;
-        $_SESSION['emailUsuario'] = $usuario->email;
-        $_SESSION['fotoUsuario'] = $usuario->foto;
-        $_SESSION['senhaUsuario'] = $usuario->senha;
+        $_SESSION['id'] = $usuario->id;
+        $_SESSION['nome'] = $usuario->nome;
+        $_SESSION['email'] = $usuario->email;
+        $_SESSION['foto'] = $usuario->imagem;
+        $_SESSION['rg'] = $usuario->rg;
+        $_SESSION['cpf'] = $usuario->cpf;
+        $_SESSION['senha'] = $usuario->senha;
+        $_SESSION['tipo'] = $usuario->tipo;
         header('location: home.php');
     }else{
         header('location: index.php?status=error');
