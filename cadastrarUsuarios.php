7c96197256e7247aea3832cc0921b1f81dbcef5f
carlosjonas <carlosjonasfon@gmail.com>
2024-03-13 22:59:01 -0300

    refactor: colocando o header de convidados

--- cadastrarUsuarios.php
+++ cadastrarUsuarios.php
@@ -10,34 +10,34 @@ $usuario = new Usuario();
 // Validação de campos do formulário 
 if (isset($_POST['nome'],$_POST['email'],$_POST['cpf'],$_POST['senha'])) {
 
+    try{
+        //Definindo a imagem da notícia
+        $dir = "image/";
+        if(isset($_FILES['imagem'])){
 
-	//Definindo a imagem da notícia
-	$dir = "image/";
-    if(isset($_FILES['imagem'])){
-        $file = $_FILES['imagem'];
-        $destino = "$dir".$file['name'];
-        move_uploaded_file($file['tmp_name'],$destino);
-        
-        
-        $usuario->imagem = $destino;
+            $file = $_FILES['imagem'];
+            $destino = "$dir".$file['name'];
+            move_uploaded_file($file['tmp_name'],$destino);    
+            $usuario->imagem = $destino;
+        }
+
+        //Inserção de campos
+        $usuario->nome = $_POST['nome'];
+        $usuario->email = $_POST['email'];
+        $usuario->rg = $_POST['rg'];
+        $usuario->cpf = $_POST['cpf'];
+        $usuario->senha = $_POST['senha'];
+        $usuario->tipo = 'l';
+        $usuario->cadastrar();
+
+        header('location: index.php?status=success');
+
+    }catch(Exception $e){
+        header('location: index.php?status=error');
     }
-
-    //Inserção de campos
-	$usuario->nome = $_POST['nome'];
-	$usuario->email = $_POST['email'];
-	$usuario->rg = $_POST['rg'];
-    $usuario->cpf = $_POST['cpf'];
-    $usuario->senha = $_POST['senha'];
-    $usuario->tipo = 'l';
-	$usuario->cadastrar();
-
-	header('location: index.php?status=success');
-
-}else{
-    header('location: index.php?status=error');
 }
 
-include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/headerConvidado.php';
 include __DIR__.'/includes/formularioUsuarios.php';
 include __DIR__.'/includes/footer.php';
 
