13d2b179c38ab7ea036b839197f888d490129085
carlosjonas <carlosjonasfon@gmail.com>
2024-03-14 23:04:27 -0300

    refactor: mudando o layout das notícias

--- includes/listagem.php
+++ includes/listagem.php
@@ -3,62 +3,71 @@
 <?php 
 	// Verificando o status da url para mandar a mensagem de sucesso
 	$mensagem = '';
-			if (isset($_GET['status'])) {
-				switch ($_GET['status']) {
-					case 'success':
-						$mensagem= '<div class="alert alert-success text-center" id="alert">Ação executada com sucesso!
-										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-									</div>';
-						break;
-					case 'error':
-						$mensagem= '<div class="alert alert-danger text-center">Ação não executada!<
-										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-									/div>';
-						break;
-				}
+	
+		if (isset($_GET['status'])) {
+			switch ($_GET['status']) {
+				case 'success':
+					$mensagem= '<div class="alert alert-success text-center" id="alert">Ação executada com sucesso!
+									<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+								</div>';
+					break;
+				case 'error':
+					$mensagem= '<div class="alert alert-danger text-center">Ação não executada!<
+									<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+								/div>';
+					break;
 			}
+		}
  ?>	
  
-	<section>
+	<section class="mt-3">
 		<?= $mensagem; ?>
-		<a href="cadastrar.php">
-			<button class="btn btn-success">Nova Notícia</button>
-		</a>
+		<?php if($tipo != "l"){ ?>
+			<a href="cadastrar.php">
+				<button class="btn btn-success">Nova Notícia</button>
+			</a>
+		<?php } ?>
 	</section>
 
 	<section>
-		<?php 
-			$resultados = '';
-			$resultados = strlen($resultados) ? $resultados : '<p>Nenhuma Nótícia publicada!</p>';
-			foreach ($noticia as $noticias):
-		?>	
-
-			<div class="card mb-3 mt-4" style="width: 100%;">
-			  <div class="row g-0">
-			    <div class="col-md-4">
-			      <img src="<?= $noticias->imagem ;?>" id="imagem" class="img-fluid rounded-start" alt="">
-			    </div>
-			    <div class="col-md-8">
-			      <div class="card-body text-dark" id="info_noticia">
-			        <h5 class="card-title"><?= $noticias->id ;?> - <?= $noticias->titulo ;?></h5>
-			        <p class="card-text"><?= $noticias->descricao ;?></p>
-			        <p class="card-text"><small class="text-muted"><?= date('d/m/Y à\s H:i:s',strtotime($noticias->data)) ;?></small></p>
-			      </div>
-			      <div class="card-body">
-			      	<a href="editar.php?id='<?= $noticias->id ;?>'">
-			  			<button type="button" class="btn btn-primary">Editar</button>
-				  	</a>
-				  	<a href="excluir.php?id='<?= $noticias->id ;?>'">
-				  		<button type="button" class="btn btn-danger">Excluir</button>
-				  	</a>
-			      </div>
-			    </div>
-			  </div>
-			</div>
-		<?php 
-			endforeach;
+		<div class="row justify-content-evenly">
+			<?php 
+				$resultados = '';
+				$resultados = strlen($resultados) ? $resultados : '<p>Nenhuma Nótícia publicada!</p>';
+				foreach ($noticia as $noticias):
+					if($noticias->ativo != "n"){
+			?>	
 			
- 		?>	
+			<div class="card mt-4 col-5" style="max-width: 540px;">
+				<a class="linkCardNoticia" href="visualizadorDeNoticias.php?id='<?= $noticias->id ;?>'">
+					<div class="row">
+						<div class="col-md-4 p-0">
+							<img src="<?= $noticias->imagem ;?>" id="imagem" class="img-fluid rounded-start" alt="imagem da notícia">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body text-dark" id="info_noticia">
+								<h5 class="card-title row" style="min-height:80px"><?= $noticias->titulo ;?></h5>
+								<p class="card-text row"><small class="text-muted p-0"><?= date('d/m/Y',strtotime($noticias->data)) ;?></small></p>
+								<?php if($tipo != "l"){ ?>
+									<a href="editar.php?id='<?= $noticias->id ;?>'">
+										<button type="button" class="btn btn-primary">Editar</button>
+									</a>
+									<a href="excluir.php?id='<?= $noticias->id ;?>'">
+										<button type="button" class="btn btn-danger">Excluir</button>
+									</a>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+			  	</a>
+			</div>
+			
+			<?php 
+					}
+				endforeach;
+				
+			?>	
+		</div>
 	</section>
 
 </main>
