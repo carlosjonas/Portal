8f3e87e3af1579c7b87f1057db902784750bc635
carlosjonas <carlosjonasfon@gmail.com>
2024-03-17 21:59:27 -0300

    refactor: trazendo as informações dos comentários

--- visualizadorDeNoticias.php
+++ visualizadorDeNoticias.php
@@ -5,9 +5,16 @@
 	define('TITLE','Editar Notícia');
 
 	use \App\Entity\Noticia;
+	use \App\Entity\Comentario;
 
 	//Consulta a notícia
 	$noticia = Noticia::getNoticia($_GET['id']);
+
+	$query = "	SELECT C.*, U.nome, U.imagem FROM comentarios AS C
+				INNER JOIN usuarios AS U ON U.id = C.idUsuario
+				WHERE idNoticia = ".$_GET['id'];
+	//Pegando os comentários
+	$comentarios = Comentario::getComentarios(null,null,null,$query);
 
 	//Validar a notícia
 	if(!$noticia instanceof Noticia){
