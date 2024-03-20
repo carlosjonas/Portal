728d57702dc62d70a621d15f9eed4e0e8d70fc12
carlosjonas <carlosjonasfon@gmail.com>
2024-03-19 21:20:52 -0300

    feature: adicionando funções e requisições para os models

--- includes/noticia.php
+++ includes/noticia.php
@@ -14,52 +14,237 @@
             <div class="row mt-3"><h6><small class="text-muted p-0">Notícia publicada dia <?= date('d/m/Y à\s H:i:s',strtotime($noticia->data)) ;?></small></h6></div>
 
 
-            <h6 class="mt-5">Comentários:</h6>
-            <form method="POST" action="processaComentario.php">
+            <?php if(isset($_SESSION['id'])){?>
+            <form method="POST" class="mt-5">
                 <textarea name="comentario" id="comentario" class="form-control inputComentario" minlength="5" rows="3" required></textarea>
                 <input type="hidden" name="idNoticia" value="<?=$noticia->id?>">
                 <input type="hidden" name="idUsuario" value="<?=$id?>">
-                <button type="submit" class="btn corSite text-light mt-3">Comentar</button>
+                <a onclick="cadastrarComentario()"  class="btn corSite mt-3">Comentar</a>
             </form>
-
-
+            <?php }else{?>
+                <h5>Logue para comentar nesta notícia!</h5>
+            <?php }?>
             <hr class="mt-3" />
-
-            <ul class="list-group ">
-
-            <?php 
-				foreach ($comentarios as $comentario):
-			?>	
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div>
-                        <img class="imagemComentario" src="<?=$comentario->imagem?>">
-                    </div>
-                    <div class="ms-2 me-auto col">
-                        <div class="divComentario">
-                            <div class="fw-bold"><h4><?=$comentario->nome?></h4></div>
-                            <?=$comentario->comentario?>
-                            <div>
-                                <label for="resposta<?=$comentario->id?>"><h6>Resposta:</h6></label>
-                                <textarea class="form-control inputComentario" id="resposta<?=$comentario->id?>" name="resposta<?=$comentario->id?>" rows="2"></textarea>
-                            </div>
-                        </div>
-                       
-                    </div>
-                    <div>
-                        <span class="badge corSite rounded-pill"><?= date('d/m/Y',strtotime($comentario->data)) ;?></span>
-                        <?php if($comentario->idUsuario == $id){ ?>
-                            <div class="mt-2">
-                                <a class="btn corSite" href="#" title="Editar" role="button"><i class="bi bi-pencil"></i></a>
-                                <a class="btn corSite" href="#" title="Excluir" role="button"><i class="bi bi-trash"></i></a>
-                            </div>
-
-                        <?php } ?>
-                    </div>
-                </li>
-                
-            <?php 
-				endforeach
-			?>
-            </ul>
+            <h4 class="mt-3">Comentários:</h4>
+            <ul id="list-Group" class="list-group"></ul>
         </div>
     </div>
+
+    <script>
+        window.onload = function () {
+            getComentarios();
+        }
+
+        idNoticia = "<?=$_GET['id']?>";
+
+        //Função que muda o comentário para textarea
+        function mudarParaTextarea(id){
+
+            //Mudando texto para textarea
+            inputTexto = document.getElementById("comentario"+id);
+            paragrafo = document.getElementById("paragrafo"+id);
+            html = "<textarea id='textarea"+id+"' class='form-control me-2' rows='3'>"+paragrafo.innerHTML+"</textarea>"
+            inputTexto.innerHTML = html;
+            
+            //Foco
+            textarea = document.getElementById("textarea"+id);
+            textarea.focus()
+            
+            //Mudando botão
+            trocarBotãoEditar(id)
+        }
+
+        function trocarBotãoEditar(id){
+            btnEditar = document.getElementById("btnEditar"+id);
+            btnEditar.innerHTML = '<i class="bi bi-floppy"></i>';
+            btnEditar.setAttribute("onclick", "editarComentario("+id+")");
+        }
+
+        function cadastrarComentario(){
+            textarea = document.getElementById("comentario");
+            comentario = textarea.value
+            idUsuario = "<?=$_SESSION['id']?>";
+            idNoticia = "<?=$_GET['id']?>";
+            if(comentario !=""){
+                url = "./Controller/comentario_controller.php?action=cadastrarComentario&idUsuario="+idUsuario+"&comentario="+comentario+"&idNoticia="+idNoticia;
+                const xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function(){
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+
+                        // Atribuindo a response text a json para ficar mais semantico
+                        try{
+                            let json = JSON.parse(this.responseText);
+                            if (json == true ) {
+                                btn = document.getElementById("btnCloseModal")
+                                btn.click();
+                                getComentarios();
+                                textarea.value = "";
+                            }
+                        }catch(error){
+                            mostrarModalAviso('Erro ao cadastrar o comentário!',true)
+                            
+                        }
+                    }
+                }
+                xhttp.open("GET", url);
+                xhttp.send();
+            }
+        }
+
+        //Função que faz uma requisição para pegar os comentários
+        function getComentarios(){
+            url = "./Controller/comentario_controller.php?action=getComentario&id="+idNoticia;
+		    const xhttp = new XMLHttpRequest();
+		    xhttp.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+
+                    // Atribuindo a response text a json para ficar mais semantico
+                    let json = JSON.parse(this.responseText);
+
+                    //Inicializando o txt para receber a estrutura de card
+                    let txt = '';
+                    
+                    let lista = document.getElementById("list-Group")
+
+                    if (json == '' ) {
+                        txt += '<h4 class="text-center">Nenhum comentário para essa notícia</h4>'
+                    }else if(json != '' ){
+                        console.log(json)
+                        json.forEach(function(comentario){
+                            spliter = comentario.data.split(" ")
+                            data = spliter[0]
+                            datasplit = data.split("-");
+                            data = datasplit[2]+"-"+datasplit[1]+"-"+datasplit[0];
+                            txt +='<li class="list-group-item d-flex justify-content-between align-items-start">'
+                            txt +='    <div>'
+                            txt +='        <img class="imagemComentario" src="'+comentario.imagem+'">'
+                            txt +='    </div>'
+                            txt +='    <div class="ms-2 me-auto col">'
+                            txt +='        <div class="divComentario">'
+                            txt +='            <div class="fw-bold"><h4>'+comentario.nome+'</h4></div>'
+                            txt +='            <div id="comentario'+comentario.id+'">'
+                            txt +='               <p id="paragrafo'+comentario.id+'">'+comentario.comentario+'</p>'
+                            txt +='            </div>'
+                            txt +='        </div>'
+                            txt +='   </div>'
+                            txt +='     <div class="ms-2">'
+                            txt +='        <span class="badge corSite text-dark rounded-pill">'+data+'</span>'
+                            txt +='            <div class="mt-2">'
+                            txt +='                <a class="btn corSite" title="Editar" id="btnEditar'+comentario.id+'" onclick="mudarParaTextarea('+comentario.id+')"><i class="bi bi-pencil"></i></a>'
+                            txt +='                <a class="btn corSite" title="Excluir" onclick="mostrarModalAviso(\'Tem certeza que deseja excluir esse comentário ?\',false,'+comentario.id+')"><i class="bi bi-trash"></i></a>'
+                            txt +='            </div>'
+                            txt +='     </div>'
+                            txt +='</li>'
+                        });
+
+                    }
+                    lista.innerHTML = txt;
+                }
+		    }
+		    xhttp.open("GET", url);
+		    xhttp.send();
+
+        }
+
+        //Função que faz uma requisição para pegar os comentários
+        function editarComentario(id){
+            comentario = document.getElementById("textarea"+id).value
+            url = "./Controller/comentario_controller.php?action=editarComentario&id="+id+"&comentario="+comentario;
+		    const xhttp = new XMLHttpRequest();
+		    xhttp.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+
+                    // Atribuindo a response text a json para ficar mais semantico
+                    try{
+                        let json = JSON.parse(this.responseText);
+                        if (json == true ) {
+                            mudarParaParagrafo(id);
+                        }
+                    }catch(error){
+                        mostrarModalAviso('Erro ao editar o comentário!',true)
+                        
+                    }
+                }
+		    }
+		    xhttp.open("GET", url);
+		    xhttp.send();
+
+        }
+
+        //Função que faz uma requisição para pegar os comentários
+        function deletarComentario(id){
+            
+            url = "./Controller/comentario_controller.php?action=deletarComentario&id="+id;
+		    const xhttp = new XMLHttpRequest();
+		    xhttp.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+
+                    // Atribuindo a response text a json para ficar mais semantico
+                    try{
+                        let json = JSON.parse(this.responseText);
+                        if (json == true ) {
+                            btn = document.getElementById("btnCloseModal")
+                            btn.click();
+                            getComentarios();
+
+                        }
+                    }catch(error){
+                        mostrarModalAviso('Erro ao deletar o comentário!',true)
+                        
+                    }
+                }
+		    }
+		    xhttp.open("GET", url);
+		    xhttp.send();
+
+        }
+
+        //Função que mostra o modal com um aviso
+        function mostrarModalAviso(texto,desaparecer,id){
+            modal = document.getElementById("my-modal")
+            modal.style.display = 'block';
+            document.getElementById("paragrafoAviso").innerHTML= texto
+
+            if(desaparecer == true){
+                setTimeout(() => {
+                    modal.style.display = 'none';
+                }, 3000);
+            }else{
+                btnAviso = document.getElementById("btnSalvarAviso")
+                btnAviso.style.display="block"
+                btnAviso.setAttribute("onclick", "deletarComentario("+id+")");
+
+            }
+        }
+
+
+
+        //Função que muda o comentário para textarea
+        function mudarParaParagrafo(id){
+
+            //Mudando texto para textarea
+            inputTexto = document.getElementById("comentario"+id);
+            textarea = document.getElementById("textarea"+id);
+            html = '<p id="paragrafo'+id+'">'+textarea.value+'</p>'
+            inputTexto.innerHTML = html;
+
+            //Mudando botão
+            trocarBotãoSalvar(id)
+        }
+
+        function trocarBotãoSalvar(id){
+            btnEditar = document.getElementById("btnEditar"+id);
+            btnEditar.innerHTML = '<i class="bi bi-pencil"></i>';
+            btnEditar.setAttribute("onclick", "mudarParaTextarea("+id+")");
+        }
+
+        //função que faz o modal fechar ao clicar no btn-close
+        function fechaModal(){
+            modal = document.getElementById("my-modal")
+            modal.style.display = 'none';
+        }
+    </script>
